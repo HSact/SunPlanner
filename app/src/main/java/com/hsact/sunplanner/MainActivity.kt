43f3cbbf34faa99dc03969a942ee602d253c2b19
Alex <actorlefter@gmail.com>
2025-04-02 00:21:01 +0300

    refactor: Update MainActivity and introduce LazyColumn
    This commit updates the `MainActivity` to utilize a `LazyColumn`.
    - Replaced the previous `Text` element in `MainScreen` with a `LazyColumn`.

--- app/src/main/java/com/hsact/sunplanner/MainActivity.kt
+++ app/src/main/java/com/hsact/sunplanner/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -40,7 +41,10 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen(name: String, modifier: Modifier = Modifier) {
-    Text(text = "Hello $name!", modifier = modifier)
+    LazyColumn{
+
+    }
+    //Text(text = "Hello $name!", modifier = modifier)
 }
 
 @Preview(showBackground = true)
