3d1bbc42fb539c69d8281cd2ca9aee5aa30e5974
Alex <actorlefter@gmail.com>
2025-04-03 03:58:37 +0300

    refactor: Simplify search bar and remove unused code
    This commit refactors the `MainScreenUI` by removing unnecessary elements and simplifying the search bar.
    - Removed unused imports and code related to `KeyboardArrowDown` and `KeyboardArrowUp`.
    - Removed unused `RowScope` parameter.
    - Removed unused `Arrangement` parameter in `Column`.
    - Removed unused `IconButton` element.
    - Adjusted layout structure.

--- app/src/main/java/com/hsact/sunplanner/mainscreen/MainScreenUI.kt
+++ app/src/main/java/com/hsact/sunplanner/mainscreen/MainScreenUI.kt
@@ -2,22 +2,17 @@ package com.hsact.sunplanner.mainscreen
 
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsFocusedAsState
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.KeyboardArrowDown
-import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.SearchBar
@@ -33,13 +28,11 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.hsact.sunplanner.ui.theme.SunPlannerTheme
 
 class MainScreenUI {
