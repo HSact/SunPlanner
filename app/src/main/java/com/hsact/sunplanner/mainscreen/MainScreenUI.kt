22293f8c5e303404266b62b8cec7d000ac4386db
Alex <actorlefter@gmail.com>
2025-04-04 02:45:19 +0300

    refactor: Refactor `MainScreenUI` and `SearchUI` and remove unused imports
    This commit refactors the `MainScreenUI` and `SearchUI` composables and removes multiple unused imports.
    - Cleaned up `MainScreenUI` by removing unused imports.
    - Cleaned up `SearchUI` by removing unused imports.
    - Removed unused imports in the respective files.

--- app/src/main/java/com/hsact/sunplanner/mainscreen/MainScreenUI.kt
+++ app/src/main/java/com/hsact/sunplanner/mainscreen/MainScreenUI.kt
@@ -1,30 +1,13 @@
 package com.hsact.sunplanner.mainscreen
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.interaction.collectIsFocusedAsState
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.SearchBar
-import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -32,14 +15,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.hsact.sunplanner.data.responses.Location
 import com.hsact.sunplanner.ui.theme.SunPlannerTheme
-import androidx.compose.foundation.lazy.items
-import androidx.compose.ui.zIndex
 import com.hsact.sunplanner.data.LocationUtils
 import com.hsact.sunplanner.mainscreen.searchui.SearchUI
 
--- app/src/main/java/com/hsact/sunplanner/mainscreen/searchui/SearchUI.kt
+++ app/src/main/java/com/hsact/sunplanner/mainscreen/searchui/SearchUI.kt
@@ -1,6 +1,5 @@
 package com.hsact.sunplanner.mainscreen.searchui
 
-import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsFocusedAsState
@@ -25,9 +24,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.focus.FocusRequester
