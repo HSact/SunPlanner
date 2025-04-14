f59d9f8ae688fea4542f8bbbe75f16c4e62af51f
Alex <actorlefter@gmail.com>
2025-04-15 01:40:47 +0300

    feat: Add top app bar and improve search UI
    This commit introduces a top app bar and refines the search user interface.
    - Added `TopAppBar` with the title "Sun Planner".
    - Implemented `nestedScroll` for the app bar.
    - Added search `isSearchExpanded` logic to enable/disable the dropdown pickers.
    - Refactored `MainScreenUI` layout.
    - Moved some of the UI logic from `MainActivity` to `MainScreenUI`.
    - Removed unused imports.

--- app/src/main/java/com/hsact/sunplanner/MainActivity.kt
+++ app/src/main/java/com/hsact/sunplanner/MainActivity.kt
@@ -7,8 +7,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import com.hsact.sunplanner.ui.theme.SunPlannerTheme
 import com.hsact.sunplanner.ui.mainscreen.MainScreenUI
 import com.hsact.sunplanner.ui.mainscreen.MainViewModel
@@ -21,19 +27,29 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
 
         val mainScreenUI = MainScreenUI(viewModel)
+        @OptIn(ExperimentalMaterial3Api::class)
         setContent {
             SunPlannerTheme {
+                val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
                 Scaffold(
-                    modifier = Modifier
-                    .fillMaxSize()
+                    modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+                    //.fillMaxSize(),
                     //.systemBarsPadding()
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Text("Sun Planner", style = MaterialTheme.typography.titleLarge)
+                            },
+                            scrollBehavior = scrollBehavior
+                        )
+                    }
                 ) { innerPadding ->
                     mainScreenUI.MainScreen(
                         modifier = Modifier
                             .padding(innerPadding)
-                            .fillMaxSize()
-                            //.padding(0.dp)
-                            //.padding(top = innerPadding.calculateTopPadding())
+                        //.fillMaxSize()
+                        //.padding(0.dp)
+                        //.padding(top = innerPadding.calculateTopPadding())
                         //viewModel
                     )
                 }
--- app/src/main/java/com/hsact/sunplanner/ui/mainscreen/MainScreenUI.kt
+++ app/src/main/java/com/hsact/sunplanner/ui/mainscreen/MainScreenUI.kt
@@ -1,5 +1,6 @@
 package com.hsact.sunplanner.ui.mainscreen
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -19,12 +20,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.hsact.sunplanner.ui.theme.SunPlannerTheme
 import com.hsact.sunplanner.data.utils.LocationUtils
-import com.hsact.sunplanner.domain.usecase.CreateWeatherGraphLineUseCase
 import com.hsact.sunplanner.ui.mainscreen.card.WeatherGraphLineCard
 import com.hsact.sunplanner.ui.mainscreen.searchUiKit.DropDownPicker
 import com.hsact.sunplanner.ui.mainscreen.searchUiKit.SearchUI
@@ -41,14 +40,22 @@ class MainScreenUI(val viewModel: MainViewModel) {
         var query by remember { mutableStateOf("") }
 
         val date1 = searchDataUI.startLD
-        var years1 by remember { mutableStateOf((1940 .. LocalDate.now().minusYears(1).year).toList().reversed()) }
-        val months1 by remember {mutableStateOf((1..12).toList())}
+        var years1 by remember {
+            mutableStateOf(
+                (1940..LocalDate.now().minusYears(1).year).toList().reversed()
+            )
+        }
+        val months1 by remember { mutableStateOf((1..12).toList()) }
         //val days1 by remember {mutableStateOf( (1..date1.lengthOfMonth()).toList())}
         val days1 = (1..date1.lengthOfMonth()).toList()
 
         val date2 = searchDataUI.endLD
-        val years2 by remember { mutableStateOf((1940 .. LocalDate.now().minusYears(1).year).toList().reversed()) }
-        val months2 by remember {mutableStateOf((1..12).toList())}
+        val years2 by remember {
+            mutableStateOf(
+                (1940..LocalDate.now().minusYears(1).year).toList().reversed()
+            )
+        }
+        val months2 by remember { mutableStateOf((1..12).toList()) }
         //val days2 by remember {mutableStateOf( (1..date2.lengthOfMonth()).toList())}
         val days2 = (1..date2.lengthOfMonth()).toList()
 
@@ -58,7 +65,7 @@ class MainScreenUI(val viewModel: MainViewModel) {
 
         Column(
             modifier = modifier
-                .fillMaxSize()
+                //.fillMaxSize()
                 .verticalScroll(scrollState)
         ) {
             Row(
@@ -79,125 +86,127 @@ class MainScreenUI(val viewModel: MainViewModel) {
                     onSearchExpandedChange = { isSearchExpanded = it }
                 )
             }
-            Row(
-                modifier = Modifier
-                    .padding(top = 10.dp)
-                    .fillMaxWidth()
-            ) {
-                DropDownPicker().ItemsDropdown(
-                    label = "Start year",
-                    list = years1,
-                    selected = date1.year,
-                    onSelected = {
-                        //date1.year = it
-                        //viewModel.saveDateFieldToVM(field = DateField.START_YEAR, it)},
-                        viewModel.updateStartYear(it)
-                    },
-                    modifier = Modifier.weight(0.5f)
-                )
-                DropDownPicker().ItemsDropdown(
-                    label = "End year",
-                    list = years2,
-                    selected = date2.year,
-                    onSelected = {
-                        //selectedEndYear = it
-                        //viewModel.saveDateFieldToVM(field = DateField.END_YEAR, it)
-                        viewModel.updateEndYear(it)
-                    },
+            if (!isSearchExpanded) {
+                Row(
                     modifier = Modifier
-                        .weight(0.5f)
-                        .padding(start = 5.dp)
-                )
-            }
-            Row(
-                modifier = Modifier
-                    .padding(top = 20.dp, start = 5.dp, end = 5.dp)
-            ) {
-                Card()
-                {
-                    Row(
-                        modifier = Modifier
-                            .padding(
-                                top = 10.dp, start = 5.dp,
-                                end = 5.dp, bottom = 10.dp
-                            )
-                    ) {
-                        DropDownPicker().ItemsDropdown(
-                            label = "Start month",
-                            list = months1,
-                            selected = date1.monthValue,
-                            onSelected = {
-                                //selectedStartMonth = it
-                                //viewModel.saveDateFieldToVM(field = DateField.START_MONTH, it)
-                                viewModel.updateStartMonth(it)
-                            },
-                            modifier = Modifier.weight(0.5f)
-                        )
-                        DropDownPicker().ItemsDropdown(
-                            label = "End month",
-                            list = months2,
-                            selected = date2.monthValue,
-                            onSelected = {
-                                //selectedEndMonth = it
-                                //viewModel.saveDateFieldToVM(field = DateField.END_MONTH, it)
-                                viewModel.updateEndMonth(it)
-                            },
-                            modifier = Modifier
-                                .weight(0.5f)
-                                .padding(start = 5.dp)
-                        )
-                    }
-                    Row(modifier = Modifier.padding(5.dp))
-                    {
-                        DropDownPicker().ItemsDropdown(
-                            label = "Start day",
-                            list = days1,
-                            selected = date1.dayOfMonth,
-                            onSelected = {
-                                //selectedStartDay = it
-                                //viewModel.saveDateFieldToVM(field = DateField.START_DAY, it)
-                                viewModel.updateStartDay(it)
-                            },
-                            modifier = Modifier.weight(0.5f)
-                        )
-                        DropDownPicker().ItemsDropdown(
-                            label = "End day",
-                            list = days2,
-                            selected = date2.dayOfMonth,
-                            onSelected = {
-                                //selectedEndDay = it
-                                //viewModel.saveDateFieldToVM(field = DateField.END_DAY, it)
-                                viewModel.updateEndDay(it)
-                            },
-                            modifier = Modifier
-                                .weight(0.5f)
-                                .padding(start = 5.dp)
-                        )
-                    }
-                }
-            }
-            Row(
-                modifier = Modifier
-                    .padding(top = 10.dp)
-            ) {
-                Button(
-                    onClick = { viewModel.prepareParamsForRequest() },
-                    modifier = Modifier
-                        .weight(1f)
+                        .padding(top = 10.dp)
+                        .fillMaxWidth()
                 ) {
-                    Text("Search")
+                    DropDownPicker().ItemsDropdown(
+                        label = "Start year",
+                        list = years1,
+                        selected = date1.year,
+                        onSelected = {
+                            //date1.year = it
+                            //viewModel.saveDateFieldToVM(field = DateField.START_YEAR, it)},
+                            viewModel.updateStartYear(it)
+                        },
+                        modifier = Modifier.weight(0.5f)
+                    )
+                    DropDownPicker().ItemsDropdown(
+                        label = "End year",
+                        list = years2,
+                        selected = date2.year,
+                        onSelected = {
+                            //selectedEndYear = it
+                            //viewModel.saveDateFieldToVM(field = DateField.END_YEAR, it)
+                            viewModel.updateEndYear(it)
+                        },
+                        modifier = Modifier
+                            .weight(0.5f)
+                            .padding(start = 5.dp)
+                    )
                 }
-            }
-            Row(modifier.fillMaxWidth())
-            {
-                Text("Weather: ${searchDataUI.weatherData}")
-            }
-            //Cards with weather data
-            if (searchDataUI.maxTemperature != null) {
-                WeatherGraphLineCard().WeatherCard(
-                    "Temperature",
-                    listOf(searchDataUI.maxTemperature!!, searchDataUI.minTemperature!!)
-                )
+                Row(
+                    modifier = Modifier
+                        .padding(top = 20.dp, start = 5.dp, end = 5.dp)
+                ) {
+                    Card()
+                    {
+                        Row(
+                            modifier = Modifier
+                                .padding(
+                                    top = 10.dp, start = 5.dp,
+                                    end = 5.dp, bottom = 10.dp
+                                )
+                        ) {
+                            DropDownPicker().ItemsDropdown(
+                                label = "Start month",
+                                list = months1,
+                                selected = date1.monthValue,
+                                onSelected = {
+                                    //selectedStartMonth = it
+                                    //viewModel.saveDateFieldToVM(field = DateField.START_MONTH, it)
+                                    viewModel.updateStartMonth(it)
+                                },
+                                modifier = Modifier.weight(0.5f)
+                            )
+                            DropDownPicker().ItemsDropdown(
+                                label = "End month",
+                                list = months2,
+                                selected = date2.monthValue,
+                                onSelected = {
+                                    //selectedEndMonth = it
+                                    //viewModel.saveDateFieldToVM(field = DateField.END_MONTH, it)
+                                    viewModel.updateEndMonth(it)
+                                },
+                                modifier = Modifier
+                                    .weight(0.5f)
+                                    .padding(start = 5.dp)
+                            )
+                        }
+                        Row(modifier = Modifier.padding(5.dp))
+                        {
+                            DropDownPicker().ItemsDropdown(
+                                label = "Start day",
+                                list = days1,
+                                selected = date1.dayOfMonth,
+                                onSelected = {
+                                    //selectedStartDay = it
+                                    //viewModel.saveDateFieldToVM(field = DateField.START_DAY, it)
+                                    viewModel.updateStartDay(it)
+                                },
+                                modifier = Modifier.weight(0.5f)
+                            )
+                            DropDownPicker().ItemsDropdown(
+                                label = "End day",
+                                list = days2,
+                                selected = date2.dayOfMonth,
+                                onSelected = {
+                                    //selectedEndDay = it
+                                    //viewModel.saveDateFieldToVM(field = DateField.END_DAY, it)
+                                    viewModel.updateEndDay(it)
+                                },
+                                modifier = Modifier
+                                    .weight(0.5f)
+                                    .padding(start = 5.dp)
+                            )
+                        }
+                    }
+                }
+                Row(
+                    modifier = Modifier
+                        .padding(top = 10.dp)
+                ) {
+                    Button(
+                        onClick = { viewModel.prepareParamsForRequest() },
+                        modifier = Modifier
+                            .weight(1f)
+                    ) {
+                        Text("Search")
+                    }
+                }
+                Row(modifier.fillMaxWidth())
+                {
+                    Text("Weather: ${searchDataUI.weatherData}")
+                }
+                //Cards with weather data
+                if (searchDataUI.maxTemperature != null) {
+                    WeatherGraphLineCard().WeatherCard(
+                        "Temperature",
+                        listOf(searchDataUI.maxTemperature!!, searchDataUI.minTemperature!!)
+                    )
+                }
             }
         }
     }
