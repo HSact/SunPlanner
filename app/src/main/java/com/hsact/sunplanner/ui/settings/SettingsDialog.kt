a5437d7e0d4c3deba9793463b58e445ad308472e
Alex <actorlefter@gmail.com>
2025-04-25 02:04:06 +0300

    feat: Implement theme and language settings in settings dialog
    This commit enhances the settings dialog by implementing theme and language settings options.
    - Added a theme selection using a segmented button row with "Auto," "Day," and "Night" options.
    - Added a language selection using a dropdown picker with "English" and "Russian" options.
    - Updated `SettingsDialog` to include the new `DialogContainer` to hold the settings.
    - Updated the DialogContainer to allow the user to change the theme and language.
    - Removed the placeholder text in the `SettingsDialog` and replaced it with the `DialogContainer`.

--- app/src/main/java/com/hsact/sunplanner/ui/settings/SettingsDialog.kt
+++ app/src/main/java/com/hsact/sunplanner/ui/settings/SettingsDialog.kt
@@ -1,11 +1,26 @@
 package com.hsact.sunplanner.ui.settings
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.SegmentedButton
+import androidx.compose.material3.SegmentedButtonDefaults
+import androidx.compose.material3.SingleChoiceSegmentedButtonRow
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.hsact.sunplanner.R
+import com.hsact.sunplanner.ui.mainscreen.searchUiKit.DropDownPicker
 
 class SettingsDialog {
     @Composable
@@ -19,6 +34,7 @@ class SettingsDialog {
                     Text("OK")
                 }
             },
+            modifier = Modifier.fillMaxWidth(),
             dismissButton = {
                 TextButton(onClick = onDismiss) {
                     Text(stringResource(R.string.cancel))
@@ -28,8 +44,53 @@ class SettingsDialog {
                 Text(stringResource(R.string.settings))
             },
             text = {
-                Text("Setting will be implemented soon")
+                DialogContainer()
             }
         )
+    }
+
+    @Composable
+    private fun DialogContainer() {
+        Column {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text("Theme")
+                var selectedIndex by remember { mutableIntStateOf(0) }
+                val options = listOf("Auto", "Day", "Night")
+
+                SingleChoiceSegmentedButtonRow(
+                    modifier = Modifier.padding(start = 20.dp),
+                ) {
+                    options.forEachIndexed { index, label ->
+                        SegmentedButton(
+                            shape = SegmentedButtonDefaults.itemShape(
+                                index = index,
+                                count = options.size
+                            ),
+                            onClick = { selectedIndex = index },
+                            selected = index == selectedIndex,
+                            label = { Text(label) }
+                        )
+                    }
+                }
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.CenterHorizontally),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text("Language")
+                DropDownPicker().ItemsDropdown(
+                    "",
+                    listOf<String>("English", "Russian"),
+                    selected = "English",
+                    onSelected = {},
+                    modifier = Modifier.padding(start = 20.dp)
+                )
+            }
+        }
     }
 }
