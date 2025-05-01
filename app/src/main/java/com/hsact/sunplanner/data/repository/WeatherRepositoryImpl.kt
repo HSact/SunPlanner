5b350f0859b738e75666d3321541a79fdf457cc8
Alex <actorlefter@gmail.com>
2025-05-02 01:46:55 +0300

    feat: Refactor WeatherRepository to interface and implement WeatherRepositoryImpl
    This commit refactors `WeatherRepository` into an interface and introduces `WeatherRepositoryImpl` as its implementation.
    - Created `WeatherRepository` interface to define methods for weather data fetching.
    - Implemented `WeatherRepositoryImpl` class to handle weather data retrieval using `OpenMeteoService` and `OpenMeteoGeo`.
    - Updated `NetworkModule` to provide `WeatherRepository` through dependency injection.
    - Modified `WeatherRepository` to an interface.
    - Updated string resources `strings.xml(ru)` to fix spelling mistakes.

---
+++ app/src/main/java/com/hsact/sunplanner/data/repository/WeatherRepositoryImpl.kt
@@ -0,0 +1,51 @@
+package com.hsact.sunplanner.data.repository
+
+import com.hsact.sunplanner.data.network.OpenMeteoGeo
+import com.hsact.sunplanner.data.network.OpenMeteoService
+import com.hsact.sunplanner.data.responses.Location
+import javax.inject.Inject
+
+class WeatherRepositoryImpl @Inject constructor(
+    private val service: OpenMeteoService,
+    private val geolocationService: OpenMeteoGeo
+) : WeatherRepository {
+
+    override suspend fun getWeather(
+        latitude: Double,
+        longitude: Double,
+        startDate: String,
+        endDate: String,
+        temperatureUnit: String,
+        windSpeedUnit: String,
+        precipitationUnit: String
+    ) =
+        service.getHistoricalWeather(
+            latitude,
+            longitude,
+            startDate,
+            endDate,
+            temperatureUnit = temperatureUnit,
+            windSpeedUnit = windSpeedUnit,
+            precipitationUnit = precipitationUnit
+        )
+
+    override suspend fun getCoordinatesByCity(cityName: String): Location? {
+        return try {
+            val response = geolocationService.getCityCoordinates(cityName)
+            response.results?.firstOrNull()
+        } catch (e: Exception) {
+            println("Error fetching coordinates: ${e.message}")
+            null
+        }
+    }
+
+    override suspend fun getCitiesList(cityName: String, language: String): List<Location>? {
+        return try {
+            val response = geolocationService.getCityCoordinates(cityName, language = language)
+            response.results
+        } catch (e: Exception) {
+            println("Error fetching list of cities: ${e.message}")
+            null
+        }
+    }
+}
