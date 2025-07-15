a36328f2d4a02e1a8c2ccb01439576c7f7b91660
Alex <actorlefter@gmail.com>
2025-07-16 00:59:32 +0300

    Refactor: Update JSON annotations for `DailyWeather`
    This commit updates the JSON annotations in the `DailyWeather` data class.
    The `@Json` annotation is now applied to the constructor parameters using `@param:Json` for improved clarity and to address a deprecated usage warning.
    
    - Updated `@Json` annotations to `@param:Json` for all fields in `DailyWeather`.

--- app/src/main/java/com/hsact/sunplanner/data/responses/WeatherResponse.kt
+++ app/src/main/java/com/hsact/sunplanner/data/responses/WeatherResponse.kt
@@ -13,15 +13,15 @@ data class WeatherResponse (
 @JsonClass(generateAdapter = true)
 data class DailyWeather(
     val time: List<String>,
-    @Json(name = "weather_code") val code: List<String>, //TODO: fix deprecated
-    @Json(name = "temperature_2m_max") val maxTemperature: List<Double>,
-    @Json(name = "temperature_2m_min") val minTemperature: List<Double>,
-    @Json(name = "apparent_temperature_max") val apparentMaxTemperature: List<Double>,
-    @Json(name = "apparent_temperature_min") val apparentMinTemperature: List<Double>,
-    @Json(name = "precipitation_sum") val precipitationSum: List<Double>,
-    @Json(name = "precipitation_hours") val precipitationHours: List<Double>,
-    @Json(name = "sunshine_duration") val sunshineDuration: List<Double>,
-    @Json(name = "daylight_duration") val daylightDuration: List<Double>,
-    @Json(name = "wind_speed_10m_max") val windSpeedMax: List<Double>,
-    @Json(name = "wind_gusts_10m_max") val windGustsMax: List<Double>,
+    @param:Json(name = "weather_code") val code: List<String>,
+    @param:Json(name = "temperature_2m_max") val maxTemperature: List<Double>,
+    @param:Json(name = "temperature_2m_min") val minTemperature: List<Double>,
+    @param:Json(name = "apparent_temperature_max") val apparentMaxTemperature: List<Double>,
+    @param:Json(name = "apparent_temperature_min") val apparentMinTemperature: List<Double>,
+    @param:Json(name = "precipitation_sum") val precipitationSum: List<Double>,
+    @param:Json(name = "precipitation_hours") val precipitationHours: List<Double>,
+    @param:Json(name = "sunshine_duration") val sunshineDuration: List<Double>,
+    @param:Json(name = "daylight_duration") val daylightDuration: List<Double>,
+    @param:Json(name = "wind_speed_10m_max") val windSpeedMax: List<Double>,
+    @param:Json(name = "wind_gusts_10m_max") val windGustsMax: List<Double>,
 )
